28f58253388e1f255239a938e6648d96b8610b08
John Rae <john.rae23@gmail.com>
2015-10-06 18:34:54 -0400

    More CSS editing,  finished js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-$('h2').click(toggleClass('hidden'));
+(function(){
+  $('h2').click(function(){
+    $(this).parents().siblings().addClass('hidden');
+    $(this).parents('section').removeClass('hidden');
+  })
+}());
